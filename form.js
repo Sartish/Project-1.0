4792e409bdd10cba1e24f98f9fa8757e0e63b799
Sara Carlstein <carlstein.sara@gmail.com>
2020-10-10 13:58:13 +0200

    added form validation.

--- form.js
+++ form.js
@@ -1,38 +1,56 @@
-//Declearing variables I want to use as//
-const firstName = document.getElementById('first-name');
-const lastName = document.getElementById('last-name');
-const email = document.getElementById('email');
-const phone = document.getElementById('phone');
-const message = document.getElementById('message');
+//Declearing variables I want to use in functions//
 
-//Adding an event to submit button. That something needs to happen when we click submit. 
-//The checkInput function is then running
-form.addEventListener('submit', (e) => {
+const form = document.getElementById('form');
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const phone = document.getElementById('phone');
+const email = document.getElementById('email');
+
+
+//Need to prevent the page from reloading when entering submit. Adding a event listener with a function to prevent this
+
+form.addEventListener('submit', (e) =>{
     e.preventDefault();
-//We want to chek and verify the input, if it is valid//
+
+    //cutom made function
     checkInputs();
 });
 
-//decsiding what we want the checkInput function to do.
 function checkInputs(){
-    const firstName = firstName.value();
-    const lastName = lastName.value();
-    const email = email.value();
-    const phone = phone.value();
-    const message = message.value();
+    //get values from the inputs (trim removes white space in the string.
+   
 
-
-// show error if fields are blank.
-if(firstNameValue === ''){
+    const firstnameValue = firstname.value.trim();
+    const lastnameValue = lastname.value.trim();
+    const phoneValue = phone.value.trim();
+    const emailValue = email.value.trim();
+//If they put in correct values we want to show success and wrong values, want to show erroe 
+    if(firstnameValue ===''){
         //show error
-        //add error class
-        setErrorFor(firstNameValue,'can not be blank');
-    } else {
-        // add success class
-        setSuccessFor(firstNameValue);
+        // add error class
+        setErrorFor(firstname, 'first name cannot be blank');
+    } else{
+        //add success class
+        setSucessFor(firstname);
     }
+
 }
 
+//Error function 
 
+Function setErrorFor(input, message) 
+{
+    const formControl = input.parentElement; 
+    const small = formControl.querySelector('small');
 
+    //add error message inside small
+    small.innerText = message;
 
+    //add error class
+    formControl.className = 'form-control error';
+}
+
+function SetSuccessFor (input) {
+    const formControl = input.parentElement; 
+    formControl.className = 'form-control sucess';
+}
